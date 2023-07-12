54b6909e5a80cf674dd963df524f81c024c45d7b
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-07-12 10:12:17 +0200

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,7 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server'
+  output: 'server',
+  site:'https://talingo.github.io',
+  base:'/bookshelf-astro'
 });
