b3b5d042852cfaac020f975df0f7005e5f22d162
Lautaro Llusa <llusa.lautaro@gmail.com>
2023-07-12 13:04:37 +0200

    deploy netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  site:'https://talingo.github.io',
-  base:'/bookshelf-astro'
+  site: 'https://talingo.github.io',
+  adapter: netlify()
 });
